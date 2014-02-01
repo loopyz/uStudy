ebe4f42c68d95e081e98a5bb182d47c098a97940
Angela Zhang <angelafzhang@gmail.com>
2014-01-31 19:28:44 -0500

    Made initial empty calendar view

---
+++ uStudy/uStudy/CalendarViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalendarViewController.h
+//  uStudy
+//
+//  Created by Angela Zhang on 1/31/14.
+//  Copyright (c) 2014 Angela Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalendarViewController : UICollectionView
+
+@end
---
+++ uStudy/uStudy/CalendarViewController.m
@@ -0,0 +1,31 @@
+//
+//  CalendarViewController.m
+//  uStudy
+//
+//  Created by Angela Zhang on 1/31/14.
+//  Copyright (c) 2014 Angela Zhang. All rights reserved.
+//
+
+#import "CalendarViewController.h"
+
+@implementation CalendarViewController
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
